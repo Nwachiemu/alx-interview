9e04bcc2497c68298a2095003959dd71d1c44555
Nwachiemu <emuchaynwachi@gmail.com>
2024-06-16 10:47:55 +0100

    added

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -8,9 +8,8 @@ for i in range(10000):
     sleep(random.random())
     sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
         random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
+        datetime.datetime.now().strftime("%d/%b/%Y:%H:%M:%S %z"),
         random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
         random.randint(1, 1024)
     ))
     sys.stdout.flush()
-    
--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,44 +2,42 @@
 """Log Parser"""
 import sys
 
-
 if __name__ == '__main__':
-    file_size = [0]
+    file_size = 0
     status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
                     403: 0, 404: 0, 405: 0, 500: 0}
 
     def print_stats():
         """ Print statistics """
-        print('File size: {}'.format(file_size[0]))
+        print('File size: {}'.format(file_size))
         for key in sorted(status_codes.keys()):
             if status_codes[key]:
                 print('{}: {}'.format(key, status_codes[key]))
 
     def parse_line(line):
         """ Checks the line for matches """
+        global file_size
         try:
-            line = line[:-1]
-            word = line.split(' ')
-            # File size is last parameter on stdout
-            file_size[0] += int(word[-1])
-            # Status code comes before file size
-            status_code = int(word[-2])
-            # Move through dictionary of status codes
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-        except BaseException:
+            parts = line.split()
+            if len(parts) < 7:
+                return
+            # Extract file size and status code
+            size = int(parts[-1])
+            code = int(parts[-2])
+            file_size += size
+            if code in status_codes:
+                status_codes[code] += 1
+        except Exception:
             pass
 
-    linenum = 1
+    linenum = 0
     try:
         for line in sys.stdin:
             parse_line(line)
-            """ print after every 10 lines """
+            linenum += 1
             if linenum % 10 == 0:
                 print_stats()
-            linenum += 1
     except KeyboardInterrupt:
         print_stats()
         raise
     print_stats()
-    
